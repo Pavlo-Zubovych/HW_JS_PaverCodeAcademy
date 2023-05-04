ca64dabb3b26b993375db2646249601b69ad9a1b
AntonZubovych <anton.zu.19.01@gmail.com>
2023-05-04 21:52:40 +0300

    completed the JS task

---
+++ index.js
@@ -0,0 +1,85 @@
+// Cat section
+let buttonCat = document.getElementById('buttonCat');
+let inputText = document.getElementById('inputText');
+let catImg = document.querySelector('img');
+
+buttonCat.addEventListener('click', onClik);
+buttonCat.addEventListener('dblclick', ondblclick);
+
+function onClik(i) {
+  i.preventDefault;
+
+  catImg.src = './img/Cat2.webp';
+
+  inputText.value = 'Небезпека';
+  inputText.style.color = 'red';
+  inputText.fontSize = '30px';
+
+  buttonCat.style.background = 'orange';
+  buttonCat.value = 'Заспокоїти! -> 2x click';
+}
+
+function ondblclick(i) {
+  i.preventDefault;
+
+  catImg.src = './img/Cat1.jpg';
+  inputText.value = 'Безпека)';
+  inputText.style.color = '#61be03';
+
+  buttonCat.style.background = '#61be03';
+  buttonCat.value = 'Взбодрити кіску';
+}
+
+//______________________________________________________________
+
+// // Button section v1
+// //Без this.
+// //На кожну кнопку повішений слухач подій.
+
+// let btn1 = document.getElementById('button1');
+// let btn2 = document.getElementById('button2');
+// let btn3 = document.getElementById('button3');
+// let btn4 = document.getElementById('button4');
+// let btn5 = document.getElementById('button5');
+// let btn6 = document.getElementById('button6');
+// let btn7 = document.getElementById('button7');
+
+// btn1.addEventListener('click', onClickButton);
+// btn2.addEventListener('click', onClickButton);
+// btn3.addEventListener('click', onClickButton);
+// btn4.addEventListener('click', onClickButton);
+// btn5.addEventListener('click', onClickButton);
+// btn6.addEventListener('click', onClickButton);
+// btn7.addEventListener('click', onClickButton);
+
+// function onClickButton(event) {
+//   //   console.log(event.target.textContent);
+//   alert(`Була натиснута кнопка "${event.target.textContent}"`);
+// }
+
+//______________________________________________________________
+
+// Button section v2
+// Oдин слухач подій, повішений на батьківський елемент для.
+
+let someButoon = document.querySelector('#sectionButton');
+
+someButoon.addEventListener('click', onClickButton);
+
+function onClickButton(event) {
+  //   console.log(`Була натиснута кнопка "${event.target.textContent}"`);
+
+  alert(`Була натиснута кнопка "${event.target.textContent}"`);
+}
+
+//______________________________________________________________
+
+// Button section & this
+
+function onClickBtnThis(id) {
+  let btn = document.getElementById(id);
+
+  //   console.log(btn.value);
+
+  alert(`Була натиснута кнопка "${btn.value}"`);
+}
