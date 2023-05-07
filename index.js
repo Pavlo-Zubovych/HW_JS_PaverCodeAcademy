ed187e492a895e86f33d11cf5fb1335abfc5a1ff
PavloZubovych <zu10@ukr.net>
2023-05-07 16:46:50 +0300

    fixed problems

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 // Cat section
-let buttonCat = document.getElementById('buttonCat');
-let inputText = document.getElementById('inputText');
-let catImg = document.querySelector('img');
+let buttonCat = document.getElementById("buttonCat");
+let inputText = document.getElementById("inputText");
+let catImg = document.querySelector("img");
 
-buttonCat.addEventListener('click', onClik);
-buttonCat.addEventListener('dblclick', ondblclick);
+buttonCat.addEventListener("click", onClik);
+buttonCat.addEventListener("dblclick", ondblclick);
 
 function onClik(i) {
-    i.preventDefault;
+	i.preventDefault;
 
-    catImg.src = './img/Cat2.webp';
+	catImg.src = "./img/Cat2.webp";
 
-    inputText.value = 'Небезпека';
-    inputText.style.color = 'red';
-    inputText.fontSize = '30px';
+	inputText.value = "Небезпека";
+	inputText.style.color = "red";
+	inputText.fontSize = "30px";
 
-    buttonCat.style.background = 'orange';
-    buttonCat.value = 'Заспокоїти! -> 2x click';
+	buttonCat.style.background = "orange";
+	buttonCat.value = "Заспокоїти! -> 2x click";
 }
 
 function ondblclick(i) {
-    i.preventDefault;
+	i.preventDefault;
 
-    catImg.src = './img/Cat1.jpg';
-    inputText.value = 'Безпека)';
-    inputText.style.color = '#61be03';
+	catImg.src = "./img/Cat1.jpg";
+	inputText.value = "Безпека)";
+	inputText.style.color = "#61be03";
 
-    buttonCat.style.background = '#61be03';
-    buttonCat.value = 'Взбодрити кіску';
+	buttonCat.style.background = "#61be03";
+	buttonCat.value = "Взбодрити кіску";
 }
 
 //______________________________________________________________
@@ -62,14 +62,14 @@ function ondblclick(i) {
 // Button section v2
 // Oдин слухач подій, повішений на батьківський елемент для.
 
-let someButoon = document.querySelector('#sectionButton');
+let someButoon = document.querySelector("#sectionButton");
 
-someButoon.addEventListener('click', onClickButton);
+someButoon.addEventListener("click", onClickButton);
 
 function onClickButton(event) {
-    //   console.log(`Була натиснута кнопка "${event.target.textContent}"`);
+	//   console.log(`Була натиснута кнопка "${event.target.textContent}"`);
 
-    alert(`Була натиснута кнопка "${event.target.textContent}"`);
+	alert(`Була натиснута кнопка "${event.target.textContent}"`);
 }
 
 //______________________________________________________________
@@ -77,11 +77,11 @@ function onClickButton(event) {
 // Button section & this
 
 function onClickBtnThis(id) {
-    let btn = document.getElementById(id);
+	let btn = document.getElementById(id);
 
-    //   console.log(btn.value);
+	//   console.log(btn.value);
 
-    alert(`Була натиснута кнопка "${btn.value}"`);
+	alert(`Була натиснута кнопка "${btn.value}"`);
 }
 
 //______________________________________________________________
@@ -89,19 +89,24 @@ function onClickBtnThis(id) {
 //HW 5module section
 
 // Посилання на поле введеня
-let formForText = document.getElementById('formForText');
+let formForText = document.getElementById("formForText");
 
 // Посилання на кнопки
-let simvol = querySelector('.simvol');
-let btnNumber = document.querySelector('.number');
+let simvol = document.querySelector(".simvol");
+let btnNumber = document.querySelector(".number");
 
 //Слухач подій для символів
-// simvol.addEventListener('click')
+simvol.addEventListener("click", onClickSmvl);
+
+function onClickSmvl(e) {
+	formForText.value += e.target.dataset.simvol;
+	// console.log(e.target.dataset.simvol)
+}
 
 //Слухач подій для цифр
-btnNumber.addEventListener('click', onClickBtn);
+btnNumber.addEventListener("click", onClickBtn);
 
 function onClickBtn(e) {
-    formForText.value += e.target.textContent;
-    console.log(e.target.textContent);
+	formForText.value += e.target.textContent;
+	// console.log(e.target.textContent);
 }
